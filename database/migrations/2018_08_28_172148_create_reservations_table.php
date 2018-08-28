5460ad75b95c25618dff31bdf58d6bd195228542
Adam Góra <adamgora@icloud.com>
2018-08-28 19:27:51 +0200

    Podstawowe migracje

---
+++ database/migrations/2018_08_28_172148_create_reservations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('customer_id');
+            $table->integer('car_id');
+            $table->integer('status');
+            $table->dateTime('from');
+            $table->dateTime('to');
+            $table->string('contract_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
