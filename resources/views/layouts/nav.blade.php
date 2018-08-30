86dd78d9e4c501682873cf6b86947c41e908330a
Adam Góra <adamgora@icloud.com>
2018-08-30 19:35:19 +0200

    Dodanie widokow

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            {{ config('app.name', 'Laravel') }}
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item"><a class="nav-link" href="{{ '/reservations' }}">Rezerwacje</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ '/customers' }}">Klienci</a></li>
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <li>
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/reservations/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            Jan Kowalski - SC 000AA
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col">
+                    <p><strong>Wypożyczenie od: </strong> 2018-09-01</p>
+                    <p><strong>Wypożyczenie do: </strong> 2018-09-10</p>
+                </div>
+                <div class="col">
+                    <p><strong>Status: </strong> W TRAKCIE</p>
+                    <p><strong>Koszt całkowity: </strong> 200 zł</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
